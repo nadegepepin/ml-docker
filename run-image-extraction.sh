124dc52c7e310c5b76c4f574fd5c1e66be159066
Nadege PEPIN <nadege.rique@gmail.com>
2020-10-10 23:07:39 -0400

    add refit support

--- run-image-extraction.sh
+++ run-image-extraction.sh
@@ -8,7 +8,7 @@ do
     echo "Start $start index"
     aws sagemaker create-processing-job \
 --processing-job-name image-extract-$IMG-$i \
---processing-inputs '[ {   "InputName": "input",   "S3Input": { "S3Uri": "s3://sagemaker-studio-3fqzdvw0h4y/capstone-prototype/rawdata", "LocalPath": "/opt/ml/processing/data/rawdata", "S3DataType": "S3Prefix", "S3InputMode": "File", "S3DataDistributionType": "FullyReplicated", "S3CompressionType": "None"   } }  ]' \
+--processing-inputs '[ {   "InputName": "input",   "S3Input": { "S3Uri": "s3://sagemaker-studio-3fqzdvw0h4y/capstone-prototype/data/rawdata", "LocalPath": "/opt/ml/processing/data/rawdata", "S3DataType": "S3Prefix", "S3InputMode": "File", "S3DataDistributionType": "FullyReplicated", "S3CompressionType": "None"   } }  ]' \
 --processing-resources '{ "ClusterConfig": {   "InstanceCount": 1,   "InstanceType": "ml.t3.medium",   "VolumeSizeInGB": 25 }  }' \
 --stopping-condition MaxRuntimeInSeconds=72000 \
 --app-specification ImageUri=190146978412.dkr.ecr.us-east-2.amazonaws.com/capstone-prototype:latest,ContainerEntrypoint='[python, loader.py]',ContainerArguments="[img/train,'224,224',$start,100000]" \
